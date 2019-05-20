7d5c81f6b2d6f08488ccbe5ab96e2ae6df3b5ea3
iamr0man <41066029+iamr0man@users.noreply.github.com>
2019-05-20 19:53:38 +0300

    correct conditional and add loop

--- homework1_conditional/js/script.js
+++ homework1_conditional/js/script.js
@@ -1,41 +1,22 @@
-let userName = prompt("Input your name"),
-    userAge = +prompt("Input your age");
+let userName = prompt("Your name: "), userAge = +prompt("Your age: ");
 
+debugger;
 
-if(userName === "" && !isNaN(userAge)) {
+while(userName === "" || isNaN(userAge) || userAge === 0) {
 
-    alert("You have one more attempt!");
-    userName = prompt("Input your name");
-    userAge = +prompt("Input your age");
-
-    if(userAge < 18) {
-        alert(`You are not allowed to visit this website. You have only ${userAge}`);
-    }
-    else if(userAge >= 18 && userAge <= 22) {
-        if (confirm("Are you sure you want to continue?")) {
-            alert(`Welcome, ${userName}`);
-        } else {
-            alert("You are not allowed to visit this website");
-        }
-    }
-    else {
-        alert(`Welcome, ${userName}`);
-    }
-} else {
-
-    if(userAge < 18) {
-        alert(`You are not allowed to visit this website. You have only ${userAge}`);
-    }
-    else if(userAge >= 18 && userAge <= 22) {
-        if (confirm("Are you sure you want to continue?")) {
-            alert(`Welcome, ${userName}`);
-        } else {
-            alert("You are not allowed to visit this website");
-        }
-    }
-    else {
-        alert(`Welcome, ${userName}`);
-    }
+    alert("Your data incorrect! Try again.");
+    userName = prompt("Your name: ");
+    userAge = +prompt("Your age: ");
 }
 
-
+if(userAge < 18) {
+    alert("You are not allowed to visit this website");
+} else if (userAge >= 18 && userAge <= 22) {
+    if(confirm("Are you sure you want to continue?")) {
+        alert(`Welcome, ${userName}`);
+    } else {
+        alert("You are not allowed to visit this website");
+    }
+} else {
+    alert(`Welcome, ${userName}`);
+}
